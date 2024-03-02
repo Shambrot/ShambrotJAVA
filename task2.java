6a4886015f3cf83c17b5a0bef9732f7ff8a2c6fc
username <username@gmail.com>
2024-03-02 15:20:58 +0300

    Третья задача

--- task2.java
+++ task2.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class task2 {
-    
-}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Ввод строк для сравнения
+        System.out.print("Введите первую строку: ");
+        String string1 = scanner.nextLine();
+
+        System.out.print("Введите вторую строку: ");
+        String string2 = scanner.nextLine();
+
+        // Сравнение введённых строк
+        if (string1.equals(string2)) {
+            System.out.println("Строки идентичны.");
+        } else {
+            System.out.println("Строки не идентичны.");
+        }
+    }
+}  
